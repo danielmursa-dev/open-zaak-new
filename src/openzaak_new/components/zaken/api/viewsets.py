2819a75e2715dcac708e563bd8cb2501374d40a8
Daniel Mursa <daniel@maykinmedia.nl>
2025-07-21 15:17:54 +0200

    Install commonground-api-common

--- src/openzaak_new/components/zaken/api/viewsets.py
+++ src/openzaak_new/components/zaken/api/viewsets.py
@@ -1,16 +1,28 @@
+from django.core.paginator import Paginator as DjangoPaginator
+from django.utils.functional import cached_property
+
 from rest_framework import viewsets
-from rest_framework.pagination import CursorPagination
+from rest_framework.pagination import PageNumberPagination
+from vng_api_common.pagination import DynamicPageSizeMixin
 
 from ..models import Zaak
 from .serializers import ZaakSerializer
 
 
-class ZaakCursorPagination(CursorPagination):
-    page_size = 100
-    ordering = "-id"
+class ExactPaginator(DjangoPaginator):
+    @cached_property
+    def count(self):
+        """
+        ⚡ restricts values to PK to remove implicit join from SQL query
+        """
+        return self.object_list.values("pk").count()
+
+
+class ExactPagination(DynamicPageSizeMixin, PageNumberPagination):
+    django_paginator_class = ExactPaginator
 
 
 class ZaakViewSet(viewsets.ModelViewSet):
     queryset = Zaak.objects.all()
     serializer_class = ZaakSerializer
-    pagination_class = ZaakCursorPagination
+    pagination_class = ExactPagination
