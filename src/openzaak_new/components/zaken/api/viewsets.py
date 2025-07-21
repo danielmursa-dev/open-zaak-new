4f149731b564159d52fc9d66b77ff82ca83e0163
Daniel Mursa <daniel@maykinmedia.nl>
2025-07-21 15:09:32 +0200

    CursorPagination

--- src/openzaak_new/components/zaken/api/viewsets.py
+++ src/openzaak_new/components/zaken/api/viewsets.py
@@ -1,9 +1,16 @@
 from rest_framework import viewsets
+from rest_framework.pagination import CursorPagination
 
 from ..models import Zaak
 from .serializers import ZaakSerializer
 
 
+class ZaakCursorPagination(CursorPagination):
+    page_size = 100
+    ordering = "-id"
+
+
 class ZaakViewSet(viewsets.ModelViewSet):
-    queryset = Zaak.objects.all()[0:100]
+    queryset = Zaak.objects.all()
     serializer_class = ZaakSerializer
+    pagination_class = ZaakCursorPagination
